028852780feac62cfa6e6e0799432031e35bfaa9
OMPRASAD <ommprasad24@gmail.com>
2023-11-18 20:55:56 +0530

    add new comments

--- src/main/java/com/example/makemytrip/MakemytripApplication.java
+++ src/main/java/com/example/makemytrip/MakemytripApplication.java
@@ -9,7 +9,7 @@ public class MakemytripApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MakemytripApplication.class, args);
 
-		System.out.println("this is DevOps begin");
+		System.out.println("this is DevOps begins");
 	}
 
 }
