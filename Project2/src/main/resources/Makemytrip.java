2e6489e39ac2007c1e5ad4fd9ebaa43a03f2555c
OMPRASAD <ommprasad24@gmail.com>
2023-11-19 18:37:09 +0530

    adding new branch

---
+++ Project2/src/main/resources/Makemytrip.java
@@ -0,0 +1,10 @@
+@SpringBootApplication
+public class MakemytripApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MakemytripApplication.class, args);
+
+        System.out.println("this is DevOps begins");
+    }
+
+}
--- src/main/java/com/example/makemytrip/flights.java
+++ src/main/java/com/example/makemytrip/flights.java
@@ -10,7 +10,7 @@ public class flights {
     @GetMapping("/flights")
     public String getData(){
         return
-                "Please book your hotel at 70% off";
+                "Please book your hotel at 70% of";
     }
 
     }
