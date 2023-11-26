f24aab7dcb1a56508e63ba9dc70431797b75b9f1
OMPRASAD <ommprasad24@gmail.com>
2023-11-26 21:15:01 +0530

    adding new code in script

--- src/main/java/com/example/makemytrip/MakemytripApplication.java
+++ src/main/java/com/example/makemytrip/MakemytripApplication.java
@@ -9,7 +9,7 @@ public class MakemytripApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MakemytripApplication.class, args);
 
-		System.out.println("this is DevOps begins");
+		System.out.println("this is DevOps begins lession no2 ");
 	}
 
 }
